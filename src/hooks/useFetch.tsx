a25bcdd9c8aec89e5dbf958cb816f9e86ca3b041
Willames Santos <62750897+santoswillames@users.noreply.github.com>
2023-06-22 17:34:54 -0300

    create fetch hook

---
+++ src/hooks/useFetch.tsx
@@ -0,0 +1,38 @@
+import { useCallback, useState } from 'react'
+
+const useFetch = () => {
+  const [data, setData] = useState<object | null>(null)
+  const [error, setError] = useState<string | null>(null)
+  const [loading, setLoading] = useState<boolean | null>(null)
+
+  const request = useCallback(async (url: string, options: object) => {
+    let response
+    let json
+
+    try {
+      setError(null)
+      setLoading(true)
+
+      response = await fetch(url, options)
+      json = await response.json()
+      if (response.ok === false) throw new Error(json.message)
+    } catch (error) {
+      const typedError = error as Error
+      json = null
+      setError(typedError?.message)
+    } finally {
+      setData(json)
+      setLoading(false)
+    }
+    return { response, json }
+  }, [])
+
+  return {
+    data,
+    loading,
+    error,
+    request,
+  }
+}
+
+export default useFetch
