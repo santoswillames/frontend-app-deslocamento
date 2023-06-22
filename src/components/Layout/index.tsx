23f2e6cb5d09a9822bdafdfff967bd955a17769a
Willames Santos <willamessantos@live.com>
2023-06-22 12:00:54 -0300

    crate table

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -21,7 +21,7 @@ const styles = {
     flex: ' 1 1 auto',
     overflow: 'hidden',
     paddingTop: '64px',
-    [theme.breakpoints.up('lg')]: {
+    [theme.breakpoints.up('md')]: {
       paddingLeft: '256px',
     },
   },
